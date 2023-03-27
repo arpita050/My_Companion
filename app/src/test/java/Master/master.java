6194d37ea47511f34d54d51d2f62fe54134aacd4
Arpita <arpitakundu309@gmail.com>
2023-03-28 05:09:48 +0600

    Java class added from master class

---
+++ app/src/test/java/Master/master.java
@@ -0,0 +1,26 @@
+package Master;
+
+public class master {
+    int roll;
+    String name;
+    master class(int r, String a){
+        this.roll=r;
+        this.name=a;
+    }
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
